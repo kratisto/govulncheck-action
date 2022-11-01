96a94b89d0e3d080e729962e4fd5e10b35cd32d1
Jeffrey Duroyon <jeffrey.duroyon@gmail.com>
2022-11-01 23:58:02 +0100

    rollback

--- pkg/vulncheck/runner.go
+++ pkg/vulncheck/runner.go
@@ -32,8 +32,8 @@ func NewScanner(logger zerolog.Logger, workDir string) Scanner {
 func (r *CmdScanner) Scan() (*vulncheck.Result, error) {
 	pkg := os.Getenv(envPackage)
 	r.log.Info().Msgf("Running govulncheck for package %s in dir %s", pkg, r.workDir)
-	r.log.Info().Msgf("%s %s", command, pkg)
-	cmd := exec.Command(command, pkg)
+	r.log.Info().Msgf("%s %s %s", command, flag, pkg)
+	cmd := exec.Command(command, flag, pkg)
 	cmd.Dir = r.workDir
 
 	out, cmdErr := cmd.Output()
